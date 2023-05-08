5214c343b07dc13d05fa23a2ffdf79f2c2bd4b25
vik554 <kumbharvikram27@gmail.com>
2023-05-08 15:37:01 +0530

    code added to uc1 barnch

--- src/test1/Demo1.java
+++ src/test1/Demo1.java
@@ -3,5 +3,6 @@ package test1;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("vikram kumbhar");
+        System.out.println("wlecom to java");
     }
 }
